7d7fb3a287d2cdc3f3ccd9133115161e4c8c4b9e
Artur Konopka <52220497+arturkonopka1994@users.noreply.github.com>
2020-05-04 12:12:50 +0100

    regex_password_validation.rb solution

---
+++ regex_password_validation.rb
@@ -0,0 +1,9 @@
+# You need to write regex that will validate a password to make sure it meets the following criteria:
+
+# At least six characters long
+# contains a lowercase letter
+# contains an uppercase letter
+# contains a number
+# Valid passwords will only be alphanumeric characters.
+
+regex=/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])[A-Za-z0-9]{6,}$/
